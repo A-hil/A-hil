feed942b2a8684e2d1b2803d54505f101c8a1a86
A-hil <90865313+A-hil@users.noreply.github.com>
2025-12-18 14:12:49 +0300

    Remove old demo file

--- animjs.js
+++ animjs.js
@@ -1,12 +1,1 @@
-						var wnd = document.getElementsByClassName("header__logo");
-			console.log(typeof(wnd));
-		let centerX = document.documentElement.clientWidth/2;
-		let centerY = document.documentElement.clientHeight/2;
-			wnd.style.left = centerX-wnd.offsetWidth/2 + "px";
-	function openNav() {
-		document.getElementById("mySidenav").style.width = "250px";
-	}
 
-				function closeNav() {
-			document.getElementById("mySidenav").style.width = "0";
-		}
